b508ab79ddfa346a71aa00ba0ec4921a0c73a97a
Mathe-S <sharvadzemat@gmail.com>
2025-03-06 01:34:24 +0400

    serve output.html

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,10 +1,15 @@
 import express, { Express, Request, Response } from "express";
+import path from "path";
 
 const app: Express = express();
 const port = 8000;
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello, World!");
+});
+
+app.get("/output", (req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname, "../../PS0/output.html"));
 });
 
 app.listen(port, () => {
