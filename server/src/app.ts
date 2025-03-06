56c29b0d5bc0240c1627a03290c83c283c6eb0d0
Mathe-S <sharvadzemat@gmail.com>
2025-03-07 00:23:12 +0400

    add attendance

--- server/src/app.ts
+++ server/src/app.ts
@@ -12,6 +12,27 @@ app.get("/output", (req: Request, res: Response) => {
   res.sendFile(path.join(__dirname, "../../PS0/output.html"));
 });
 
+app.get("/attendance", (req: Request, res: Response) => {
+  const { name, surname } = req.query;
+
+  if (!name || !surname) {
+    return res.status(400).json({
+      error: "Name and surname are required",
+    });
+  }
+
+  // Here we will typically save this to a database
+  // For now, we'll just send back a confirmation
+  return res.json({
+    message: "Attendance recorded",
+    student: {
+      name: name,
+      surname: surname,
+    },
+    timestamp: new Date(),
+  });
+});
+
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
 });
